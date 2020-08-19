27a6886e9b324fd1fb262cb04e8c0c8d1491b59b
Melody Cheng <melodycheng99@gmail.com>
2020-08-19 19:08:30 -0400

    Fetch shopping list names from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      listNames: [],
+      listName: null,
+      category: null,
+      items: null
+    }
+  }
+
+  componentDidMount() {
+    this.getListNames()
+  }
+
+  getListNames() {
+    axios.get('http://localhost:8000/api/shoppingLists/lists/')
+      .then(res => this.setState({ listNames: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      this.state.listName == null ? (
+        <div>
+          {this.state.listNames.join(', ')}
+        </div>
+      ) : null
+    )
+  }
 }
 
 export default App;
