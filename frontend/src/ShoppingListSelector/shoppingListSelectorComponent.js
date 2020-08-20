c02bf1c3d01734ef0e4cdd54344563720994912a
Melody Cheng <melodycheng99@gmail.com>
2020-08-19 20:59:31 -0400

    Add main screen buttons

--- frontend/src/ShoppingListSelector/shoppingListSelectorComponent.js
+++ frontend/src/ShoppingListSelector/shoppingListSelectorComponent.js
@@ -3,7 +3,7 @@ import { Dropdown } from 'semantic-ui-react'
 
 import './shoppingListSelectorComponent.css'
 
-class ShoppingListSelector extends React.Component{
+class ShoppingListSelector extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -32,16 +32,26 @@ class ShoppingListSelector extends React.Component{
     render() {
         return (
             <div className="mainScreen">
-                <span className="textClass">
-                    Select shopping list from dropdown menu, or create a new shopping list
-                </span>
-                <br />
-                <Dropdown
-                    placeholder='Select Shopping List'
-                    search
-                    selection
-                    options={this.state.shoppingListOptions}
-                />
+                <div className="dropdownContainer">
+                    <Dropdown
+                        placeholder='Select Shopping List'
+                        search
+                        selection
+                        options={this.state.shoppingListOptions}
+                    />
+                </div>
+                <button
+                    class="button goToListButton"
+                    // onClick={}
+                >
+                    Go To Selected Shopping List
+                </button>
+                <button
+                    class="button createListButton"
+                    // onClick={}
+                >
+                    Create New Shopping List
+                </button>
             </div>
         )
     }
