03489a6082b33b4ef8668ab5720e6a3d9ce7327e
Melody Cheng <melodycheng99@gmail.com>
2020-08-21 12:30:39 -0400

    Add endpoint to get items by shopping list & category

--- backend/shoppingList/views.py
+++ backend/shoppingList/views.py
@@ -16,6 +16,36 @@ class ShoppingListListView(viewsets.ModelViewSet):
     lists = ShoppingListList.objects.values_list('list_name', flat=True).distinct()
     return Response(lists)
 
+  @action(detail=True)
+  def items(self, request, *args, **kwargs):
+    list_name_id = ShoppingListList.objects.get(pk=kwargs['pk']).id
+    items = ShoppingListItem.objects.filter(list_name=list_name_id)
+    categories = items.values_list('category', flat=True).distinct()
+    items_values = items.values(
+      'category',
+      'item', 
+      'description',
+      'importance',
+      'due_date',
+      'bought'
+    )
+
+    items_response = {}
+    for category in categories:
+      category_obj = ShoppingListCategory.objects.filter(pk=category)[0]
+      category_id = category_obj.id
+      category_name = category_obj.category
+      items_response[category_name] = items.filter(category=category_id).values(
+        'category',
+        'item', 
+        'description',
+        'importance',
+        'due_date',
+        'bought'
+      )
+
+    return Response(items_response)
+
 class ShoppingListCategoryView(viewsets.ModelViewSet):
   serializer_class = ShoppingListCategorySerializer
   queryset = ShoppingListCategory.objects.all()
