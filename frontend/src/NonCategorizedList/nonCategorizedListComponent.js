696e11c3dc8d16c19e717534ca7b86a3779e3ac0
Melody Cheng <melodycheng99@gmail.com>
2020-08-24 11:10:02 -0400

    Edit categorized items

--- frontend/src/NonCategorizedList/nonCategorizedListComponent.js
+++ frontend/src/NonCategorizedList/nonCategorizedListComponent.js
@@ -12,13 +12,28 @@ class NonCategorizedList extends React.Component {
             listNameId: props.listNameId,
             category: props.category,
             items: props.items,
-            createItem: false
+            createItem: false,
+            editItemId: null,
+            editItem: null,
+            editDescription: null,
+            editDate: null
         }
+
+        this.onEdit = this.onEdit.bind(this)
     }
 
     createItem() {
         this.setState({
             createItem: true
+        })
+    }
+
+    onEdit(id, item, description, date) {
+        this.setState({
+            editItemId: id,
+            editItem: item,
+            editDescription: description,
+            editDate: date
         })
     }
 
@@ -28,11 +43,24 @@ class NonCategorizedList extends React.Component {
                 listName={this.state.listName} 
                 listNameId={this.state.listNameId} 
                 category={this.state.category} />
+        } else if (this.state.editItem != null && this.state.editItemId != null) {
+            return <CreateItem
+                listName={this.state.listName}
+                listNameId={this.state.listNameId}
+                category={this.state.category}
+                itemId={this.state.editItemId}
+                item={this.state.editItem}
+                description={this.state.editDescription}
+                date={this.state.editDate}
+                updatingItem={true} />
         } else {
             let itemViews = []
             this.state.items.forEach(item => {
                 itemViews.push(
-                    <ShoppingListItem item={item} />
+                    <ShoppingListItem 
+                        key={item.item}
+                        item={item}
+                        onEdit={this.onEdit} />
                 )
             })
 
